0e50ad25997a4c46971d12ea9440f952bbd57345
Tom Hvitved <hvitved@github.com>
2020-08-05 14:30:39 +0200

    C#: Remove assembly prefix from all extractor IDs

--- csharp/extractor/Semmle.Extraction.CSharp/Entities/Types/NamedType.cs
+++ csharp/extractor/Semmle.Extraction.CSharp/Entities/Types/NamedType.cs
@@ -17,7 +17,7 @@ namespace Semmle.Extraction.CSharp.Entities
             typeArgumentsLazy = new Lazy<Type[]>(() => symbol.TypeArguments.Select(t => Create(cx, t)).ToArray());
         }
 
-        public static NamedType Create(Context cx, INamedTypeSymbol type) => NamedTypeFactory.Instance.CreateEntityFromSymbol(cx, type);
+        public static NamedType Create(Context cx, INamedTypeSymbol type) => NamedTypeFactory.Instance.CreateEntity(cx, type);
 
         public override bool NeedsPopulation => base.NeedsPopulation || symbol.TypeKind == TypeKind.Error;
 
@@ -109,7 +109,7 @@ namespace Semmle.Extraction.CSharp.Entities
 
         public override void WriteId(TextWriter trapFile)
         {
-            symbol.BuildTypeId(Context, trapFile, true, symbol, (cx0, tb0, sub, _) => tb0.WriteSubId(Create(cx0, sub)));
+            symbol.BuildTypeId(Context, trapFile, true, symbol);
             trapFile.Write(";type");
         }
 
@@ -182,7 +182,7 @@ namespace Semmle.Extraction.CSharp.Entities
 
         public override void WriteId(TextWriter trapFile)
         {
-            referencedType.symbol.BuildNestedTypeId(Context, trapFile, referencedType.symbol);
+            trapFile.WriteSubId(referencedType);
             trapFile.Write(";typeRef");
         }
 
--- csharp/extractor/Semmle.Extraction/Context.cs
+++ csharp/extractor/Semmle.Extraction/Context.cs
@@ -49,33 +49,10 @@ namespace Semmle.Extraction
         /// <param name="factory">The entity factory.</param>
         /// <param name="init">The initializer for the entity.</param>
         /// <returns>The new/existing entity.</returns>
-        public Entity CreateEntity<Type, Entity>(ICachedEntityFactory<Type, Entity> factory, Type init) where Entity : ICachedEntity where Type:struct
-        {
-            return CreateNonNullEntity(factory, init);
-        }
-
-        /// <summary>
-        /// Creates a new entity using the factory.
-        /// </summary>
-        /// <param name="factory">The entity factory.</param>
-        /// <param name="init">The initializer for the entity.</param>
-        /// <returns>The new/existing entity.</returns>
-        public Entity CreateNullableEntity<Type, Entity>(ICachedEntityFactory<Type, Entity> factory, Type init) where Entity : ICachedEntity
-        {
-            return init == null ? CreateEntity2(factory, init) : CreateNonNullEntity(factory, init, objectEntityCache);
-        }
-
-        /// <summary>
-        /// Creates a new entity using the factory.
-        /// </summary>
-        /// <param name="factory">The entity factory.</param>
-        /// <param name="init">The initializer for the entity.</param>
-        /// <returns>The new/existing entity.</returns>
-        public Entity CreateEntityFromSymbol<Type, Entity>(ICachedEntityFactory<Type, Entity> factory, Type init)
+        public Entity CreateNullableEntity<Type, Entity>(ICachedEntityFactory<Type, Entity> factory, Type init)
             where Entity : ICachedEntity
-            where Type: ISymbol
         {
-            return init == null ? CreateEntity2(factory, init) : CreateNonNullEntity(factory, init, symbolEntityCache);
+            return init is null ? CreateEntity2(factory, init) : CreateNonNullEntity(factory, init);
         }
 
         // A recursion guard against writing to the trap file whilst writing an id to the trap file.
@@ -162,14 +139,8 @@ namespace Semmle.Extraction
         public Entity CreateNonNullEntity<Type, Entity>(ICachedEntityFactory<Type, Entity> factory, Type init)
             where Entity : ICachedEntity
             where Type : notnull
-            => CreateNonNullEntity(factory, init, objectEntityCache);
-
-        private Entity CreateNonNullEntity<Type, Entity>(ICachedEntityFactory<Type, Entity> factory, Type init, IDictionary<object, ICachedEntity> dictionary)
-            where Entity : ICachedEntity
         {
-            if (init is null) throw new ArgumentException("Unexpected null value", nameof(init));
-
-            if (dictionary.TryGetValue(init, out var cached))
+            if (objectEntityCache.TryGetValue(init, out var cached))
                 return (Entity)cached;
 
             using (StackGuard)
@@ -178,7 +149,7 @@ namespace Semmle.Extraction
                 var entity = factory.Create(this, init);
                 entity.Label = label;
 
-                dictionary[init] = entity;
+                objectEntityCache[init] = entity;
 
                 DefineLabel(entity, TrapWriter.Writer, Extractor);
                 if (entity.NeedsPopulation)
@@ -236,15 +207,16 @@ namespace Semmle.Extraction
         // Adaptor to convert IEqualityComparer<ISymbol> to IEqualityComparer<object>
         class SymbolComparer : IEqualityComparer<object>
         {
-            IEqualityComparer<ISymbol> comparer = SymbolEqualityComparer.IncludeNullability;
+            readonly IEqualityComparer<ISymbol> comparer = SymbolEqualityComparer.IncludeNullability;
 
-            bool IEqualityComparer<object>.Equals(object? x, object? y) => comparer.Equals(x as ISymbol, y as ISymbol);
+            bool IEqualityComparer<object>.Equals(object? x, object? y) =>
+                x is ISymbol sx && y is ISymbol sy ? comparer.Equals(sx, sy) : Equals(x, y);
 
-            int IEqualityComparer<object>.GetHashCode(object obj) => comparer.GetHashCode((ISymbol)obj);
+            int IEqualityComparer<object>.GetHashCode(object obj) =>
+                obj is ISymbol s ? comparer.GetHashCode(s) : obj.GetHashCode();
         }
 
-        readonly IDictionary<object, ICachedEntity> objectEntityCache = new Dictionary<object, ICachedEntity>();
-        readonly IDictionary<object, ICachedEntity> symbolEntityCache = new Dictionary<object, ICachedEntity>(10000, new SymbolComparer());
+        readonly IDictionary<object, ICachedEntity> objectEntityCache = new Dictionary<object, ICachedEntity>(10000, new SymbolComparer());
         readonly Dictionary<ICachedEntity, Label> entityLabelCache = new Dictionary<ICachedEntity, Label>();
         readonly HashSet<Label> extractedGenerics = new HashSet<Label>();
 
@@ -517,7 +489,7 @@ namespace Semmle.Extraction
             {
                 ExtractionError(message, optionalSymbol.ToDisplayString(), Entities.Location.Create(this, optionalSymbol.Locations.FirstOrDefault()));
             }
-            else if(!(optionalEntity is null))
+            else if (!(optionalEntity is null))
             {
                 ExtractionError(message, optionalEntity.Label.ToString(), Entities.Location.Create(this, optionalEntity.ReportingLocation));
             }
